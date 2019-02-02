06c40fc6838366e4352cba114345384919f1f36d
Darksang <darksang.exodar@gmail.com>
2019-02-02 20:42:39 +0100

    Added glm

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
-# rts-game
+# RTS Game
 Simple RTS game.
+
+# Compiling
+
+Create a *build* folder and run CMake from there. Build after the process finishes.
 
 # Libraries Used
 
 - GLFW 3.2.1
+- glad
+- OpenGL Mathematics 0.9.9.3
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,15 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
 
-#include <stdio.h>
+#include <iostream>
 
 int main(int argc, char * argv[]) {
    printf("Simple Program Running\n");
+
+   glm::vec4 vec(1.0f, 0.0f, 0.0f, 1.0f);
+
+   std::cout << vec.x << " " << vec.y << " " << vec.z << std::endl;
 
    return 0;
 }
